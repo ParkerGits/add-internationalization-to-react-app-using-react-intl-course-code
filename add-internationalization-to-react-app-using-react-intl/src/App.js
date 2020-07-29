e964ec3d00469ba170de0dd9d46d7c71885bcde7
Parker <parkerjlandon@gmail.com>
2020-07-28 18:19:05 -0700

    lesson 2 code

--- add-internationalization-to-react-app-using-react-intl/src/App.js
+++ add-internationalization-to-react-app-using-react-intl/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/Home";
+import BookDetail from "./components/BookDetail";
+
+import books from "./books.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <div className="Inner">
+              <Link to="/" className="Logo">
+                <img src={books} alt="Logo"/>
+                <h2>On My Shelf</h2>
+              </Link>
+            </div>
+          </div>
+
+          <div className="Container Inner">
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route path='/book/:bookId' component={BookDetail}/>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- add-internationalization-to-react-app-using-react-intl/src/messages.js
+++ add-internationalization-to-react-app-using-react-intl/src/messages.js
@@ -1,5 +1,24 @@
+
 export default {
-    'en-US': {},
-    'es-ES': {},
-    'fr-FR': {}
+  'en-US': {
+    detail: {
+      toggle: 'Toggle',
+      purchase: 'Purchase this book from:',
+      reviewsHeading: 'Reviews'
+    }
+  },
+  'es': {
+    detail: {
+      toggle: 'Palanca',
+      purchase: 'Compre este libro de:',
+      reviewsHeading: 'Comentarios'
+    }
+  },
+  'fr': {
+    detail: {
+      toggle:'Basculer',
+      purchase: 'Achetez ce livre à partir de:',
+      reviewsHeading: 'Avis'
+    }
   }
+}
