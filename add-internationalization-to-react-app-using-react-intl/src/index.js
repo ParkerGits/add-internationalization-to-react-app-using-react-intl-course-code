2414839c6921414059b475203b9c9d595e922f12
Parker <parkerjlandon@gmail.com>
2020-08-01 07:02:58 -0700

    lesson 11 code

--- add-internationalization-to-react-app-using-react-intl/src/index.js
+++ add-internationalization-to-react-app-using-react-intl/src/index.js
@@ -8,15 +8,34 @@ import { flattenMessages } from './utils';
 
 import App from './App';
 import './index.css';
+if (!window.Intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/fr.js',
+    'intl/locale-data/jsonp/es.js'
+  ], (require) => {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    require('intl/locale-data/jsonp/fr.js');
+    require('intl/locale-data/jsonp/es.js');
+  })
+  runApp()
+} else {
+  runApp()
+}
 
-let locale = (navigator.languages && navigator.languages[0])
-             || navigator.language
-             || navigator.userLanguage
-             || 'en-US';
+function runApp() {
+  let locale = (navigator.languages && navigator.languages[0])
+  || navigator.language
+  || navigator.userLanguage
+  || 'en-US';
 
-ReactDOM.render(
-  <IntlProvider locale={locale} defaultLocale="en-US" messages={flattenMessages(messages[locale])}>
+  ReactDOM.render(
+    <IntlProvider locale={locale} defaultLocale="en-US" messages={flattenMessages(messages[locale])}>
     <App />
-  </IntlProvider>,
+    </IntlProvider>,
   document.getElementById('root')
-);
+  );
+}
+
